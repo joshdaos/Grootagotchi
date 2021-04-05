5bfd0969898419724f7f5d003b34f7c7f54eb5c1
JoshDaos <joshuadaos1998@gmail.com>
2021-04-05 16:51:15 -0700

    working on mvp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
- TAMAGOTCHI PROJECT ZERO - Josh Daos
+ GROOTAGOTCHI PROJECT ZERO - Josh Daos
 
+## Welcome to Grootagotchi
+
+1. What we have here is a Tamagotchi game, The Pet is named "Grootagotchi".
+Right now he is still a seedling so we need to make him grow.
+Water him daily to see him grow.
+Within a timeframe of consistently watering him he'll evolve into baby Grootgotchi!
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-console.log("SANITY CHECK!")
+console.log("Grootagaotchi!")
+
+/* MVP
+-Goal is to evolve groot through some stages 
+-Grow seedling Groot by watering him consistently for a specific amount of time.
+-After specific time groot will sprout to baby groot 
+-if groot doesn't get watered he will die
+-after groot sprouts, he needs to be cared for
+-feed (water/fertilizer) 
+-love (spend time?)
+-sleep (for rest)
+*/
