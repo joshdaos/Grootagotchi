9e4a227bdae9039e5d056352cb522d39a83cbf79
JoshDaos <joshuadaos1998@gmail.com>
2021-04-06 14:09:10 -0700

    Working on stubbing it up

--- README.md
+++ README.md
@@ -2,16 +2,18 @@
 
 ## Welcome to Grootagotchi
 
-1. What we have here is a Tamagotchi game, The Pet is named "Grootagotchi".
+- What we have here is a Tamagotchi game, The Pet is named "Groot".
 Right now he is still a seedling so we need to make him grow.
 Water him daily to see him grow! 
 Your goal is to gorw him to baby Groot and not let him die.
 
-2. Within a timeframe of consistently watering him he'll evolve into baby Grootgotchi!
+- Within a timeframe of consistently watering him he'll evolve into Baby Groot!
 If we don't tend to him he'll eventually die from lack of hunger.
 
-3. Once he is sprouted he'll become baby groot!
+- Once he is sprouted he'll become baby groot!
 Now that he is sprouted he'll need more than just water..
-He needs food, love, and sleep!
+He needs food, attention, and sleep!
+
+- Carefully manage him by consistently giving him food, attention, and sleep!
 
 
--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,60 @@
-console.log("Grootagaotchi!")
+console.log("Grootagatchi!")
+ 
+// Create a repo for your tomagotchi pet ~
+// make a commit after you finish each one of the following ~
+// Instatiate your Tomagotchi
+// Display a character of your choice on the screen to represent your pet ~
+// Display the following metrics for your pet:
+// Hunger (1-10 scale)
+// Sleepiness (1-10 scale)
+// Boredom (1-10 scale)
+// Age
+// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+// Add the ability to name your pet.
+// Style the page.
+// Increase your pet's age every x minutes
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Morph your pet at certain ages.
+// Animate your pet across the screen while it's alive.
 
 /* MVP
+Brief overview
 -Goal is to evolve groot through some stages 
 -Grow seedling Groot by watering him consistently for a specific amount of time.
 -After specific time groot will sprout to baby groot 
 -if groot doesn't get watered he will die
--after groot sprouts, he needs to be cared for
+-after groot sprouts into baby groot, he needs to be cared for a little more 
 -feed (water/fertilizer) 
--love (spend time?)
+-attention (spend time?)
 -sleep (for rest)
+
+-When user clicks start, the first meter appears, and seedling needs to be watered
+-When user clicks the start (button) at the top of the body's page, the meter will begin
+-to water the seedling press a button/icon that waters the seedling
+-water about 30s, if button is nt pressed when meter goes down, the game is over
+-if consisitently watered seedling changes into a baby groot
+-after change, 3 other similar meters pop up 
+- along with meters 3 other buttons pop up
+-all 3 meters are the same but have about 20s or 15s to be press,
+- if one meter goes down game over, groot dies,
+-all buttons have to be managed consistently for the game to keep going
+-goal is just to keep groot alive as long as possible
 */
+
+// 1. Step One 
+// build character in object 
+// stub it up
+
+class Tamagotchi {
+    constructor (name) {
+        this.name = name;
+        this.hunger = hunger;
+        this.attention = attention;
+        this.sleep = sleep;
+    }
+}
+
+const groot = new Tamagotchi(Groot);
+
+
